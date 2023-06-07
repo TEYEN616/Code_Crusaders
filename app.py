4ffa44c16323a33cc7a65ea2b19ec52ed9e6ee64
Tim Hötzel <tafeljunior@gmail.com>
2023-06-07 10:32:15 +0200

    Tims erweitrung app.py Fragen und erstes Model

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
 import os
 from flask import Flask, render_template, redirect, url_for, request
-
+import random
 
 app = Flask(__name__)
+
+# Fragen und Antworten in einer Liste
+quiz = [
+    {
+        "question": "Was ist die Hauptstadt von Frankreich?",
+        "options": ["Madrid", "Paris", "Berlin", "London"],
+        "answer": "Paris"
+    },
+    {
+        "question": "Wie viele Planeten hat unser Sonnensystem?",
+        "options": ["7", "8", "9", "10"],
+        "answer": "8"
+    },
+    {
+        "question": "Was ist die Hauptstadt von Kanada?",
+        "options": ["Ottawa", "Toronto", "Montreal", "Vancouver"],
+        "answer": "Ottawa"
+    }
+] 
 
 app.config.from_mapping(
     SECRET_KEY='secret_key_just_for_dev_environment',
@@ -13,6 +32,22 @@ app.config.from_mapping(
 @app.route('/')
 def index():
     return redirect(url_for('logIn'))
+
+@app.route('/gameloop', methods=['Get', 'POST'])
+def quiz_app():
+    if request.method == 'POST':
+        user_answer = request.form['answer']
+        question_index = int(request.form.get('question_index', 0))
+        question = quiz[question_index]
+        if user_answer == "":
+            return render_template('gameloop.html', question=question, message="Zeit abgelaufen! Die Frage wurde übersprungen.")
+        elif question["options"][int(user_answer) - 1] == question["answer"]:
+            return render_template('gameloop.html', question=question, message="Richtig!", score=1)
+        else:
+            return render_template('gameloop.html', question=question, message=f"Falsch! Die richtige Antwort ist {question['answer']}.")
+    else:
+        random.shuffle(quiz)
+        return render_template('gameloop.html', question=quiz[0])
 
 
 @app.route('/check_answer', methods=['POST'])
