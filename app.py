1f178ff637216b96995c585b426a58f78d53f7e7
Tim Hötzel <tafeljunior@gmail.com>
2023-07-07 10:42:44 +0200

    Messages working

--- app.py
+++ app.py
@@ -1,25 +1,86 @@
 import os
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session
 import random
 
 app = Flask(__name__)
 
 # Fragen und Antworten in einer Liste
 quiz = [
+    
     {
-        "id" : 1,
+        "id": 1,
+        "question": "Wie heißt die größte Wüste der Welt?",
+        "options": ["Gobi-Wüste", "Atacama-Wüste", "Sahara-Wüste", "Taklamakan-Wüste"],
+        "answer": "3"
+    },
+    {
+        "id": 2,
+        "question": "Welches ist das längste Flussystem der Welt?",
+        "options": ["Nil", "Mississippi", "Amazonas", "Jangtse"],
+        "answer": "3"
+    },
+    {
+        "id": 3,
+        "question": "Welche Farbe hat die Schale einer Zitrone?",
+        "options": ["Grün", "Gelb", "Orange", "Weiß"],
+        "answer": "2"
+    },
+    {
+        "id": 4,
+        "question": "Welche ist die größte Insel der Welt?",
+        "options": ["Grönland", "Neuguinea", "Borneo", "Madagaskar"],
+        "answer": "1"
+    },
+    {
+        "id": 5,
+        "question": "Welcher Planet ist der vierte in unserem Sonnensystem?",
+        "options": ["Mars", "Venus", "Jupiter", "Saturn"],
+        "answer": "1"
+    },
+    {
+        "id": 6,
+        "question": "Wie viele Elemente enthält das Periodensystem?",
+        "options": ["98", "105", "118", "124"],
+        "answer": "3"
+    },
+    {
+        "id": 7,
+        "question": "Welches Land hat die längste Küstenlinie der Welt?",
+        "options": ["Russland", "Kanada", "Australien", "Brasilien"],
+        "answer": "2"
+    },
+    {
+        "id": 8,
+        "question": "Wie viele Kontinente gibt es auf der Erde?",
+        "options": ["5", "6", "7", "8"],
+        "answer": "3"
+    },
+    {
+        "id": 9,
+        "question": "Wer hat die Relativitätstheorie entwickelt?",
+        "options": ["Isaac Newton", "Albert Einstein", "Max Planck", "Nikola Tesla"],
+        "answer": "2"
+    },
+    {
+        "id": 10,
+        "question": "Wie viele Milliliter sind in einem Liter?",
+        "options": ["100", "500", "1000", "1500"],
+        "answer": "3"
+    },
+    {
+        "id" : 11,
         "question": "Was ist die Hauptstadt von Frankreich?",
         "options": ["Madrid", "Paris", "Berlin", "London"],
         "answer": "2"
     },
     {
-        "id" : 2,
+        "id" : 12,
         "question": "Wie viele Planeten hat unser Sonnensystem?",
         "options": ["7", "8", "9", "10"],
         "answer": "2"
     },
     {
-        "id" : 3,
+        "id" : 13,
         "question": "Was ist die Hauptstadt von Kanada?",
         "options": ["Ottawa", "Toronto", "Montreal", "Vancouver"],
         "answer": "1"
@@ -36,23 +97,30 @@ app.config.from_mapping(
 def index():
     return redirect(url_for('gameloop', question_id = 1)) ##logIn
 
+
 @app.route('/gameloop/<int:question_id>', methods=['GET', 'POST'])
 def gameloop(question_id):
     question = next((q for q in quiz if q["id"] == question_id), None)
+    
     if question:
         if request.method == 'POST' and 'answer' in request.form:
             user_answer = request.form['answer']
             correct_answer = question["answer"]
+            
             if str(user_answer) == str(correct_answer):
                 message = "Richtig!"
-                return redirect(url_for('gameloop', question_id=question_id + 1, message=message))
+                next_question_id = question_id + 1
+                next_question = next((q for q in quiz if q["id"] == next_question_id), None)
+                if next_question:
+                    return render_template('gameloop.html', question=next_question, question_index=next_question_id, message=message)
+                else:
+                    return redirect(url_for('index'))
             else:
                 message = "Falsch!"
-                return redirect(url_for('gameloop', question_id=question_id, message=message))
+                return render_template('gameloop.html', question=question, question_index=question_id, message=message)
         else:
             message = ""
-            return render_template('gameloop.html', question=question, question_index=question_id, message="Dies ist eine Testnachricht")
-
+            return render_template('gameloop.html', question=question, question_index=question_id, message=message)
     else:
         return redirect(url_for('index'))
 
@@ -64,8 +132,6 @@ def signup():
             username= request.form.get('username')
             password= request.form.get('password')
             password= request.form.get('confirm_password')
-
-            # hier können wir die daten die hier gerade erfasst wurden verarbeiten. In der DB z.B. 
 
             return redirect(url_for('index'))
         
