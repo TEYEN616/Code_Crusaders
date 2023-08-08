20e52403778ca2aadc3e472c35c2464fbe74fa15
Tim Hötzel <tafeljunior@gmail.com>
2023-08-08 19:31:34 +0200

    kleinigkeiten und die beiden mode html Seiten

--- Fragen.py
+++ Fragen.py
@@ -1,9 +1,10 @@
 import sqlite3
 
-conn = sqlite3.connect('quiz.db')
+conn = sqlite3.connect("quiz.db")
 cursor = conn.cursor()
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS questions (
+cursor.execute(
+    """CREATE TABLE IF NOT EXISTS questions (
                     id INTEGER PRIMARY KEY,
                     question TEXT,
                     option1 TEXT,
@@ -12,88 +13,100 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS questions (
                     option4 TEXT,
                     answer INTEGER,
                     answered INTEGER DEFAULT 0
-                )''')
-#Tabelle für user erstellen
-cursor.execute('''
+                )"""
+)
+# Tabelle für user erstellen
+cursor.execute(
+    """
                    CREATE TABLE IF NOT EXISTS users (
                    id INTEGER PRIMARY KEY AUTOINCREMENT,
                    email TEXT NOT NULL,
                    username TEXT NOT NULL,
                    password TEXT NOT NULL
                     )
-                   ''')
+                   """
+)
 
 
-#Neu -----------------------------------------
-cursor.execute('''CREATE TABLE IF NOT EXISTS scores (
+# Neu -----------------------------------------
+cursor.execute(
+    """CREATE TABLE IF NOT EXISTS scores (
                     id INTEGER PRIMARY KEY,
                     score INTEGER
-                )''')
-#Neu -----------------------------------------
+                )"""
+)
+# Neu -----------------------------------------
 questions = [
     {
-        'id': 1,
-        'question': 'Wie heißt die Hauptstadt von Frankreich?',
-        'option1': 'Madrid',
-        'option2': 'Paris',
-        'option3': 'Berlin',
-        'option4': 'London',
-        'answer': 2
+        "id": 1,
+        "question": "Wie heißt die Hauptstadt von Frankreich?",
+        "option1": "Madrid",
+        "option2": "Paris",
+        "option3": "Berlin",
+        "option4": "London",
+        "answer": 2,
     },
     {
-        'id': 2,
-        'question': 'Wie viele Planeten hat unser Sonnensystem?',
-        'option1': '7',
-        'option2': '8',
-        'option3': '9',
-        'option4': '10',
-        'answer': 2
+        "id": 2,
+        "question": "Wie viele Planeten hat unser Sonnensystem?",
+        "option1": "7",
+        "option2": "8",
+        "option3": "9",
+        "option4": "10",
+        "answer": 2,
     },
     {
-        'id': 3,
-        'question': 'Was ist die Hauptstadt von Kanada?',
-        'option1': 'Ottawa',
-        'option2': 'Toronto',
-        'option3': 'Montreal',
-        'option4': 'Vancouver',
-        'answer': 1
+        "id": 3,
+        "question": "Was ist die Hauptstadt von Kanada?",
+        "option1": "Ottawa",
+        "option2": "Toronto",
+        "option3": "Montreal",
+        "option4": "Vancouver",
+        "answer": 1,
     },
     {
-        'id': 4,
-        'question': 'Wie viele Kontinente gibt es auf der Erde??',
-        'option1': '5',
-        'option2': '6',
-        'option3': '7',
-        'option4': '8',
-        'answer': 3
+        "id": 4,
+        "question": "Wie viele Kontinente gibt es auf der Erde??",
+        "option1": "5",
+        "option2": "6",
+        "option3": "7",
+        "option4": "8",
+        "answer": 3,
     },
     {
-        'id': 5,
-        'question': 'Wie viele Milliliter sind in einem Liter?',
-        'option1': '100',
-        'option2': '500',
-        'option3': '1000',
-        'option4': '2000',
-        'answer': 3
+        "id": 5,
+        "question": "Wie viele Milliliter sind in einem Liter?",
+        "option1": "100",
+        "option2": "500",
+        "option3": "1000",
+        "option4": "2000",
+        "answer": 3,
     },
     {
-        'id': 6,
-        'question': 'Wie viele Elemente enthält das Periodensystem',
-        'option1': '98',
-        'option2': '105',
-        'option3': '118',
-        'option4': '124',
-        'answer': 3
+        "id": 6,
+        "question": "Wie viele Elemente enthält das Periodensystem",
+        "option1": "98",
+        "option2": "105",
+        "option3": "118",
+        "option4": "124",
+        "answer": 3,
     },
-
 ]
 
 for question in questions:
-    cursor.execute('''INSERT INTO questions (id, question, option1, option2, option3, option4, answer, answered)
-                      VALUES (?, ?, ?, ?, ?, ?, ?, 0)''',
-                   (question['id'], question['question'], question['option1'], question['option2'], question['option3'], question['option4'], question['answer']))
-    
-
+    cursor.execute(
+        """INSERT INTO questions (id, question, option1, option2, option3, option4, answer, answered)
+                      VALUES (?, ?, ?, ?, ?, ?, ?, 0)""",
+        (
+            question["id"],
+            question["question"],
+            question["option1"],
+            question["option2"],
+            question["option3"],
+            question["option4"],
+            question["answer"],
+        ),
+    )
 
 conn.commit()
 conn.close()
--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template, redirect, url_for, request, session
-from flask_wtf import FlaskForm
-from wtforms.fields import StringField, SubmitField, PasswordField, EmailField, StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length, DataRequired, Email, EqualTo
-import sqlite3
 import random
+import sqlite3
+
+from flask import Flask, redirect, render_template, request, session, url_for
+from flask_wtf import FlaskForm
+from wtforms.fields import EmailField, PasswordField, StringField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, InputRequired, Length
 
 app = Flask(__name__)
 
+"""
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max= 15),])
     password = PasswordField('password', validators=[InputRequired(), Length (min=8, max=80)])
@@ -17,55 +19,64 @@ class SignUpForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Sign Up')
+"""
+
 
 def reset_quiz():
-    conn = sqlite3.connect('quiz.db')
+    conn = sqlite3.connect("quiz.db")
     cursor = conn.cursor()
 
     # Markierung der beantworteten Fragen entfernen
-    cursor.execute('UPDATE questions SET answered = 0')
+    cursor.execute("UPDATE questions SET answered = 0")
     conn.commit()
 
     # Verbindung zur Datenbank schließen
     conn.close()
 
+
 def initialize_score():
-    conn = sqlite3.connect('quiz.db')
+    conn = sqlite3.connect("quiz.db")
     cursor = conn.cursor()
 
     # Tabelle 'scores' erstellen, wenn sie nicht existiert
-    cursor.execute('''CREATE TABLE IF NOT EXISTS scores (
+    cursor.execute(
+        """CREATE TABLE IF NOT EXISTS scores (
                         id INTEGER PRIMARY KEY,
                         score INTEGER DEFAULT 0
-                    )''')
+                    )"""
+    )
 
     # Prüfen, ob bereits ein Eintrag in der Tabelle vorhanden ist
-    cursor.execute('SELECT * FROM scores')
+    cursor.execute("SELECT * FROM scores")
     existing_score = cursor.fetchone()
 
     if not existing_score:
         # Kein Eintrag vorhanden, wir setzen den Punktestand auf 0
-        cursor.execute('INSERT INTO scores (score) VALUES (?)', (0,))
+        cursor.execute("INSERT INTO scores (score) VALUES (?)", (0,))
         conn.commit()
 
     conn.close()
 
-#User in DB einsetzen
+
+# User in DB einsetzen
 def insert_user(email, username, password):
-        conn = sqlite3.connect('quiz.db')
-        cursor = conn.cursor()
-        cursor.execute('INSERT INTO users (email, username, password) VALUES (?, ?, ?)', (email, username, password))
-        conn.commit()
-        conn.close()
+    conn = sqlite3.connect("quiz.db")
+    cursor = conn.cursor()
+    cursor.execute(
+        "INSERT INTO users (email, username, password) VALUES (?, ?, ?)",
+        (email, username, password),
+    )
+    conn.commit()
+    conn.close()
 
 
 # Score aktualisieren
 def update_score():
-    conn = sqlite3.connect('quiz.db')
+    conn = sqlite3.connect("quiz.db")
     cursor = conn.cursor()
 
     # Punktestand aus der Datenbank abrufen
-    cursor.execute('SELECT score FROM scores')
+    cursor.execute("SELECT score FROM scores")
     current_score = cursor.fetchone()
 
     if current_score:
@@ -73,15 +84,22 @@ def update_score():
         score = current_score[0] + 1
 
         # Punktestand in der Datenbank aktualisieren
-        cursor.execute('UPDATE scores SET score = ?', (score,))
+        cursor.execute("UPDATE scores SET score = ?", (score,))
         conn.commit()
 
     conn.close()
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    return redirect(url_for('logIn'))
 
+@app.route("/", methods=["GET", "POST"])
+def index():
+    # return redirect(url_for('logIn'))
+    if request.method == "POST":
+        return redirect("/quiz")
+
+    return render_template("homepage.html")
+
+
+"""
 @app.route('/signup', methods=['GET','POST'])
 def signup():
         form=SignUpForm()
@@ -94,27 +112,31 @@ def signup():
 
         else:   
             return render_template ('signup.html', form=form)
-        
+"""
 
-@app.route('/homepage', methods=['GET', 'POST'])
+
+@app.route("/homepage", methods=["GET", "POST"])
 def homepage():
+    """
     if request.method=='POST':
         return redirect(url_for('/quiz'))
     else:
         return render_template('homepage.html')
+    """
+    return render_template("homepage.html")
 
 
-@app.route('/quiz', methods=['GET', 'POST'])
+@app.route("/quiz", methods=["GET", "POST"])
 def quiz():
-    conn = sqlite3.connect('quiz.db')
+    conn = sqlite3.connect("quiz.db")
     cursor = conn.cursor()
 
-    if request.method == 'POST':
-        question_id = request.form['question_id']
-        user_answer = request.form['answer']
+    if request.method == "POST":
+        question_id = request.form["question_id"]
+        user_answer = request.form["answer"]
 
         # Frage aus der Datenbank abrufen
-        cursor.execute('SELECT * FROM questions WHERE id = ?', (question_id,))
+        cursor.execute("SELECT * FROM questions WHERE id = ?", (question_id,))
         question = cursor.fetchone()
 
         if question and user_answer == str(question[6]):
@@ -126,12 +148,12 @@ def quiz():
         message = None
 
     # Punktestand aus der Datenbank abrufen
-    cursor.execute('SELECT score FROM scores')
+    cursor.execute("SELECT score FROM scores")
     current_score = cursor.fetchone()
     score = current_score[0] if current_score else 0
 
     # Alle unbeantworteten Fragen aus der Datenbank abrufen
-    cursor.execute('SELECT * FROM questions WHERE answered = 0')
+    cursor.execute("SELECT * FROM questions WHERE answered = 0")
     questions = cursor.fetchall()
 
     if not questions:
@@ -139,76 +161,76 @@ def quiz():
         reset_quiz()
 
         # Punktestand auf 0 zurücksetzen
-        cursor.execute('UPDATE scores SET score = 0')
+        cursor.execute("UPDATE scores SET score = 0")
         conn.commit()
 
         conn.close()
 
-        return redirect('/homepage')  # Weiterleitung zur Homepage
-
-        # Homepage-Seite anzeigen
-        # return render_template('homepage.html')
+        return redirect("/homepage")  # Weiterleitung zur Homepage
 
     # Eine zufällige Frage auswählen
     question = random.choice(questions)
 
     # Frage als beantwortet markieren
-    cursor.execute('UPDATE questions SET answered = 1 WHERE id = ?', (question[0],))
+    cursor.execute("UPDATE questions SET answered = 1 WHERE id = ?", (question[0],))
     conn.commit()
 
     conn.close()
 
-    return render_template('quiz.html', question=question, message=message, score=score)
+    return render_template(
+        "rushquiz.html", question=question, message=message, score=score
+    )
 
-@app.route('/gkquiz', methods=['GET', 'POST'])
+
+@app.route("/gkquiz", methods=["GET", "POST"])
 def gkquiz():
     print("Entered gkquiz route.")
-    conn = sqlite3.connect('quiz.db')
+    conn = sqlite3.connect("quiz.db")
     cursor = conn.cursor()
-    
-    if request.method == 'POST':
+
+    if request.method == "POST":
         print("Form submitted.")
-        question_id = request.form['question_id']
-        user_answer = request.form['answer']
+        question_id = request.form["question_id"]
+        user_answer = request.form["answer"]
         # Frage aus der Datenbank abrufen
-        cursor.execute('SELECT * FROM questions WHERE id = ?', (question_id,))
+        cursor.execute("SELECT * FROM questions WHERE id = ?", (question_id,))
         question = cursor.fetchone()
-        
+
         if question and user_answer == str(question[6]):
             message = "Richtig!"
         else:
             message = "Falsch!"
     else:
         message = None
-   
+
     # Alle unbeantworteten Fragen aus der Datenbank abrufen
-    cursor.execute('SELECT * FROM questions WHERE answered = 0')
+    cursor.execute("SELECT * FROM questions WHERE answered = 0")
     questions = cursor.fetchall()
-    
+
     if not questions:
         # Alle Fragen wurden beantwortet, das Quiz startet von vorne
         reset_quiz()
 
         # Punktestand auf 0 zurücksetzen
-        cursor.execute('UPDATE scores SET score = 0')
+        cursor.execute("UPDATE scores SET score = 0")
         conn.commit()
 
         conn.close()
 
-        return redirect('/homepage')  # Weiterleitung zur Homepage
+        return redirect("/homepage")  # Weiterleitung zur Homepage
 
-    #Eine zufällige Frage auswählen
+    # Eine zufällige Frage auswählen
     question = random.choice(questions)
 
-    #Frage als beantwortet markieren
-    cursor.execute('UPDATE questions SET answered = 1 WHERE id = ?', (question[0],))
+    # Frage als beantwortet markieren
+    cursor.execute("UPDATE questions SET answered = 1 WHERE id = ?", (question[0],))
     conn.commit()
 
     conn.close()
 
-    return render_template('genQuiz.html', question=question, message=message)
+    return render_template("gkQuiz.html", question=question, message=message)
 
 
-if __name__ == '__main__':
-    initialize_score()  #Punktestand initialisieren
+if __name__ == "__main__":
+    initialize_score()  # Punktestand initialisieren
     app.run()
